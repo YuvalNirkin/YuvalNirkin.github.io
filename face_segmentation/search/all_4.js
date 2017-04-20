250023b6d4e25bf240f38e8a97ccab94a6775537
Yuval Nirkin <Yuval Nirkin>
2017-04-21 00:21:14 +0300

    Added documentation for face_segmentation

---
+++ face_segmentation/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['init',['init',['../classface__swap_1_1_face_renderer.html#abfdf9e1d2944450dbb24505580742912',1,'face_swap::FaceRenderer']]],
+  ['issegmentationmodelinit',['isSegmentationModelInit',['../classface__swap_1_1_face_swap.html#a9172cdc6c97e8de9ac0ebfcb2b4a8fea',1,'face_swap::FaceSwap']]]
+];
